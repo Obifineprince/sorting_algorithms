98595b33593b2f69eb1a8148a259f5681590f432
Obifineprince <ifeanyiprincewill070@gmail.com>
2023-07-29 11:33:38 +0000

    function sorts an array of integers in ascending order using count algorithim

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,71 +2,69 @@
 #include <stdlib.h>
 
 /**
- * count_array - Creates the count array.
- * @array: Array of integers.
- * @size: Size of the array.
- * @k: Largest number in the unsorted array.
- * Return: The count array.
+ * count_array - creates the count array
+ * @array: array of integers
+ * @size: size of array
+ * @k: largest number in unsorted array
+ * Return: the count array
  */
 int *count_array(int *array, size_t size, int k)
 {
-    int *count_array;
-    size_t i, j;
+    int *ca, i;
+    size_t j;
 
-    count_array = malloc((k + 1) * sizeof(int));
-    if (count_array == NULL)
+    ca = malloc((k + 1) * sizeof(int));
+    if (ca == NULL)
         return (NULL);
 
     for (i = 0; i < (k + 1); i++)
-        count_array[i] = 0;
+        ca[i] = 0;
 
     for (j = 0; j < size; j++)
-        count_array[array[j]]++;
+        ca[array[j]]++;
 
-    for (i = 1; i <= k; i++)
-        count_array[i] = count_array[i] + count_array[i - 1];
-
-    print_array(count_array, k + 1);
-
-    return (count_array);
+    print_array(ca, k + 1);
+    return (ca);
 }
 
 /**
- * counting_sort - Sorts an array of integers in ascending order
- *                 using the Counting sort algorithm.
- * @array: Array of integers.
- * @size: Size of the array.
+ * counting_sort - sorts an array of integers in ascending order
+ * using the Counting sort algorithm
+ * @array: array of integers
+ * @size: size of array
  */
 void counting_sort(int *array, size_t size)
 {
-    int *temp_array, max_num, *count_array;
-    size_t i, j;
+    int *b, k, *ca, j;
+    size_t i;
 
     if (array == NULL || size < 2)
         return;
 
-    max_num = array[0];
-    for (i = 1; i < size; i++) {
-        if (array[i] > max_num)
-            max_num = array[i];
+    k = array[0];
+    for (i = 1; i < size; i++)
+    {
+        if (array[i] > k)
+            k = array[i];
     }
 
-    count_array = count_array(array, size, max_num);
-    if (count_array == NULL)
+    ca = count_array(array, size, k);
+    if (ca == NULL)
         return;
 
-    temp_array = malloc(size * sizeof(int));
-    if (temp_array == NULL) {
-        free(count_array);
+    b = malloc(size * sizeof(int));
+    if (b == NULL)
+    {
+        free(ca);
         return;
     }
 
     for (j = size - 1; j >= 0; j--)
-        temp_array[--count_array[array[j]]] = array[j];
+        b[--ca[array[j]]] = array[j];
 
     for (i = 0; i < size; i++)
-        array[i] = temp_array[i];
+        array[i] = b[i];
 
-    free(temp_array);
-    free(count_array);
+    free(b);
+    free(ca);
 }
